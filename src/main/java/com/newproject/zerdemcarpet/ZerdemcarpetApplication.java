10b73e608327741046d8b19884efec454c9ecac2
cener78 <94464442+cener78@users.noreply.github.com>
2022-09-15 23:29:30 +0200

    Initial commit

---
+++ src/main/java/com/newproject/zerdemcarpet/ZerdemcarpetApplication.java
@@ -0,0 +1,13 @@
+package com.newproject.zerdemcarpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerdemcarpetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerdemcarpetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newproject/zerdemcarpet/ZerdemcarpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newproject.zerdemcarpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerdemcarpetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
